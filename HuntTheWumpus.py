f0064403f77609b2a725e93ca02de16d0d1cbfe9
Py-Mike <28496247+Py-Mike@users.noreply.github.com>
2021-05-11 16:07:11 -0400

    Added functions
    Refactored the code to feature functions. Also assigned all variables to the game dictionary

--- HuntTheWumpus.py
+++ HuntTheWumpus.py
@@ -3,114 +3,128 @@
 #Started: 5/10/2021
 #Description: A Hunt the Wumpus Clone in Python
 
+#Imports
 import random
 
-#initialize variables
-arrows = 5
-gameover = False
-action_count = 0
-bat_locations = [0,0]
-pit_locations = [0,0]
-room = {1: [2,5,8],}
-room[2] = [1,3,10]
-room[3] = [2,4,12]
-room[4] = [3,5,15]
-room[5] = [1,4,6]
-room[6] = [5,7,15]
-room[7] = [6,8,17]
-room[8] = [1,7,9]
-room[9] = [8,10,18]
-room[10] = [2,9,11]
-room[11] = [10,12,19]
-room[12] = [3,11,13]
-room[13] = [12,14,20]
-room[14] = [4,13,15]
-room[15] = [6,14,16]
-room[16] = [15,17,20]
-room[17] = [7,16,18]
-room[18] = [9,17,19]
-room[19] = [11,18,20]
-room[20] = [13,16,19]
+def game_setup():
+    #Create a dictionary with all the game values
+    game = {'gameover': False, 'arrows': 5, 'player_location': 0, 'wumpus_location': 0,
+            'bat1_location': 0, 'bat2_location': 0, 'pit1_location': 0, 'pit2_location': 0,
+            1: (2,5,8), 2: (1,3,10), 3: (2,4,12), 4: (3,5,15), 5: (1,4,6), 6: (5,7,15),
+            7: (6,8,17), 8: (1,7,9), 9: (8,10,18), 10: (2,9,11), 11: (10,12,19),
+            12: (3,11,13), 13: (12,14,20), 14: (4,13,15), 15: (6,14,16), 16: (15,17,20),
+            17: (7,16,18), 18: (9,17,19), 19: (11,18,20), 20: (13,16,19)}
 
-#Randomize Starting Locations
-locations = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
-random.shuffle(locations)
-player_location  = locations[0]
-wumpus_location = locations[1]
-bat1_location = locations[2]
-bat2_location = locations[3]
-pit1_location = locations[4]
-pit2_location = locations[5]
+    #Randomize Starting Locations
+    locations = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+    random.shuffle(locations)
 
-print('Developer Mode:')
-print('Wumpus at ' + str(wumpus_location))
-print('Bats at ' + str(bat1_location) + ' & ' + str(bat2_location))
-print('Pits at ' + str(pit1_location) + ' & ' + str(pit2_location))
-#Game Loop
-while gameover == False:
-    #Game Screen Output
-    print('You have taken ' + str(action_count) + ' actions.')
-    print('You are in room ' + str(player_location) + '.')
-    if wumpus_location in room[player_location]:
+    #Assign key locations to their respective game variables
+    game['player_location'] = locations[0]
+    game['wumpus_location'] = locations[1]
+    game['bat1_location'] = locations[2]
+    game['bat2_location'] = locations[3]
+    game['pit1_location'] = locations[4]
+    game['pit2_location'] = locations[5]
+    return game
+    
+def game_screen(game):
+    #Prints the room details
+    print('You are in room ' + str(game['player_location']) + '.')
+    if game['wumpus_location'] in game[game['player_location']]:
         print('    I smell a Wumpus.')
-    if bat1_location in room[player_location]:
+    if game['bat1_location'] in game[game['player_location']]:
         print('    I hear flapping.')
-    if bat2_location in room[player_location]:
+    if game['bat2_location'] in game[game['player_location']]:
         print('    I hear flapping.')
-    if pit1_location in room[player_location]:
+    if game['pit1_location'] in game[game['player_location']]:
         print('    I feel a draft.')
-    if pit2_location in room[player_location]:
+    if game['pit2_location'] in game[game['player_location']]:
         print('    I feel a draft.')
-    print('Tunnels lead to ' + str(room[player_location][0]) +
-          ', ' + str(room[player_location][1]) +
-          ', ' + str(room[player_location][2]))
-    #Prompt Player for Action
-    action = input('Shoot or Move (S-M)?')
-    action = action.lower()
-    if action == 's':
-        arrow_range = input ('How far? [1-5]')
-        arrow_range = int(arrow_range)
-        for a in range(arrow_range):
-            room_check = input('Which room #: ')
-            if int(room_check) == wumpus_location:
-                print('You have slain the wumpus. Congratulations!')
-                gameover = True
-                break
-            a += 1
-        print('You did not hit anything.')
-        if wumpus_location in room[player_location]:
-            print('You hear shuffling, and the scent of wumpus grows fainter.')
-            new_wumpus_location = random.randint(1,19)
-            if new_wumpus_location >= player_location:
-                wumpus_location = new_wumpus_location + 1
-            else:
-                wumpus_location = new_wumpus_location
+    print('Tunnels lead to ' + str(game[game['player_location']][0]) +
+          ', ' + str(game[game['player_location']][1]) +
+          ', ' + str(game[game['player_location']][2]))
+    
+def shoot(game):
+    #First Main Action
+    #needs to kill the wumpus, scare the wumpus, chance to ricochet and fly through several rooms
+    arrow_range = input ('How far? [1-5]')
+    arrow_range = int(arrow_range)
+    for a in range(arrow_range):
+        room_check = input('Which room #: ')
+        if int(room_check) == game['wumpus_location']:
+            print('You have slain the wumpus. Congratulations!')
+            gameover = True
+            return
+        
+        else:
+            print('You did not hit anything.')
+            if wumpus_location in game[player_location]:
+                print('You hear shuffling, and the scent of wumpus grows fainter.')
+                new_wumpus_location = random.randint(1,19)
+                if new_wumpus_location >= game['player_location']:
+                    game['wumpus_location'] = new_wumpus_location + 1
+                else:
+                    game['wumpus_location'] = new_wumpus_location
+                        
         arrows -= 1
         print('You have ' + str(arrows) + ' arrows remaining.')
-    elif action == 'm':
-        move = input('Where to? ')
-        #if move not in room[player_location]:
-        #    move = inp
-        player_location = int(move)
+        a += 1
 
-        #Check for hazards
-        #I check bats first since they update the player location
-        if (player_location == bat1_location) or (player_location == bat2_location):
-            print('The bats swarm and carry you to a random location.')
-            player_location = random.randint(1,20)
-        if player_location == wumpus_location:
-            wumpus_reaction = random.randint(1,2)
-            if wumpus_reaction == 1:
-                print('You have angered the wumpus. It attacks. Game over')
-                gameover = True
-            else:
-                print('You startled the wumpus. It ran away.')
-                wumpus_location = random.randint(1,20)
-        if (player_location == pit1_location) or (player_location == pit2_location):
-            print('You fell into a bottomless pit. Game over.')
-            gameover = True
+def move(game):
+    #Second Main Action
+    #needs to move through the map, and check to see if bad things happen in the next room
+    move = input('Where to? ')
+
+    #Validate input to prevent players from moving to locations not currently available
+    if int(move) not in game[game['player_location']]:
+        print('You bump into the wall. That is not a valid exit!')
+        return
     else:
-        print('Incorrect Syntax')
+        game['player_location'] = int(move)
 
-    print('')
-    action_count += 1
+    #Check for Hazards
+    #Check bats first since they update the player location
+    if (game['player_location'] == game['bat1_location']) or (game['player_location'] == game['bat2_location']):
+        print('The bats swarm and carry you to a random location.')
+        game['player_location'] = random.randint(1,20)
+    if game['player_location'] == game['wumpus_location']:
+        wumpus_reaction = random.randint(1,2)
+        if wumpus_reaction == 1:
+            print('You have angered the wumpus. It attacks. Game over')
+            game['gameover'] = True
+        else:
+            print('You startled the wumpus. It ran away.')
+            game['wumpus_location'] = random.randint(1,20)
+    if (game['player_location'] == game['pit1_location']) or (game['player_location'] == game['pit2_location']):
+        print('You fell into a bottomless pit. Game over.')
+        game['gameover'] = True
+    
+def game():
+    #Initialize Game
+    game = game_setup()
+
+    #Start Game Loop
+    while game['gameover'] == False:
+        #Print game screen
+        game_screen(game)
+        
+        #Prompt Player for Action
+        action = input('Shoot or Move (S-M)?')
+        action = action.lower()
+        if action == 's':
+            #take the shooting action
+            shoot(game)
+        elif action == 'm':
+            #take the moving action
+            move(game)
+        else:
+            print('Incorrect Syntax')
+
+        print('')
+    
+def main():
+    #Calls the main game function
+    game()
+
+main()
